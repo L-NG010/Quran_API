8a916254577df96c48887715758ad3ccb72d50bf
unknown <langnatanegara73@gmail.com>
2025-04-15 13:31:53 +0700

    first commit

--- app/Http/Controllers/ChapterController.php
+++ app/Http/Controllers/ChapterController.php
@@ -43,7 +43,7 @@ class ChapterController extends Controller
             'verses_count'=>$chapter->verses_count,
             'page_number'=>$page_number,
             'juz_number'=>$juz_number,
-            'translated_name'=>json_decode($chapter->translated_name),
+            'translated_name'=>json_encode($chapter->translated_name),
             'scripts'=>$formattedScripts
         ]);
     }
