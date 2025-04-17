bc3d2390fb76246607a408762a8dc24f0e8631ee
unknown <langnatanegara73@gmail.com>
2025-04-17 14:35:06 +0700

    Api Baru

--- app/Models/Word.php
+++ app/Models/Word.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Word extends Model
 {
-    //
+    protected $casts = [
+        'translation'=>'array',
+        'transliteration'=>'array'
+    ];
 }
